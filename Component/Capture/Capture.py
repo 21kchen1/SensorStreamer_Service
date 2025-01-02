de500c226c611ec106e25df6f4560282afa43789
Chen <12373718+CKgiteehub@user.noreply.gitee.com>
2025-01-02 10:04:32 +0800

    modified:   Component/Capture/Capture.py 	modified:   Test/CaptureTest.py 	modified:   View/MainView.py 更改动作类型 	modified:   View/MainView.ui 	modified:   main.py 更改动作类型

--- Component/Capture/Capture.py
+++ Component/Capture/Capture.py
@@ -1,3 +1,6 @@
+import logging
+import os
+import cv2
 
 """
     Capture 拍摄功能
@@ -5,26 +8,29 @@
     @version 1.0
 """
 
-import logging
-import cv2
-
-
 class Capture:
     # 最大分辨率
     MAX_CAP_WIDTH = 2560
     MAX_CAP_HEIGHT = 1440
     # 最大数量
-    MAX_CAP_NUM = 10
+    MAX_CAP_NUM = 3
 
     """
         设置分辨率
         @param capWidth 横向像素
         @param capHeight 纵向像素
+        @param apiPreference 硬件加速api
     """
-    def __init__(self, capWidth= MAX_CAP_WIDTH, capHeight= MAX_CAP_HEIGHT) -> None:
+    def __init__(self, capWidth= MAX_CAP_WIDTH, capHeight= MAX_CAP_HEIGHT, apiPreference= None) -> None:
         self.capWidth = min(capWidth, Capture.MAX_CAP_WIDTH)
         self.capHeight = min(capHeight, Capture.MAX_CAP_HEIGHT)
         self.cap = None
+        # 默认的接口
+        self.apiPreference = cv2.CAP_DSHOW if not os.name == 'nt' else cv2.CAP_ANY
+        if apiPreference == None:
+            return
+        # 自定义接口
+        self.apiPreference = apiPreference
 
     """
         根据下标创建相机
@@ -34,7 +40,7 @@ class Capture:
         if self.cap != None:
             return False
 
-        theCap = cv2.VideoCapture(index)
+        theCap = cv2.VideoCapture(index, self.apiPreference)
         if not theCap.isOpened():
             return False
         self.cap = theCap
@@ -50,10 +56,11 @@ class Capture:
     def create(self) -> int:
         if self.cap != None:
             return False
+
         # 记录相机下标
         index = False
         for i in range(Capture.MAX_CAP_NUM):
-            nextCap = cv2.VideoCapture(i)
+            nextCap = cv2.VideoCapture(i, self.apiPreference)
             if not nextCap.isOpened():
                 continue
             nextCap.set(cv2.CAP_PROP_FRAME_WIDTH, self.capWidth)
@@ -65,8 +72,26 @@ class Capture:
             # 比较分辨率
             if self.cap.get(cv2.CAP_PROP_FRAME_WIDTH) >= nextCap.get(cv2.CAP_PROP_FRAME_WIDTH):
                 nextCap.release()
+                continue
             self.cap.release()
             self.cap = nextCap
             index = i
-
+        logging.info(f"create : CAP_PROP_FRAME = { self.cap.get(cv2.CAP_PROP_FRAME_WIDTH) } : { self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT) }")
         return index
+
+    """
+        释放摄像头
+    """
+    def release(self) -> bool:
+        if self.cap == None:
+            return False
+        self.cap.release()
+        return True
+
+    """
+        获取设置
+    """
+    def get(self, proId: int):
+        if self.cap == None:
+            return None
+        return self.cap.get(proId)
--- Test/CaptureTest.py
+++ Test/CaptureTest.py
@@ -6,20 +6,25 @@ CAP_WIDTH = 2560
 CAP_HEIGHT = 1440
 CAP_NUM = 10
 
+
 cap = None
 cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAP_WIDTH)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAP_HEIGHT)
+if not cap.isOpened():
+    print("error")
+    exit(0)
 
 print(f"_getPicture: CAP_PROP_FRAME = { cap.get(cv2.CAP_PROP_FRAME_WIDTH) } : { cap.get(cv2.CAP_PROP_FRAME_HEIGHT) }")
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M','J','P','G'))
 cap.set(cv2.CAP_PROP_FPS, 30)
 while True:
-    # 读取每一帧
-    ret, frame = cap.read()
-    if not ret:
-        logging.warning("_getPicture: Unable to read frames")
-        continue
-    cv2.imshow(WIN_NAME, frame)
-    # 等待按键 s
-    theKey = cv2.waitKey(1)
+    continue
+#     # 读取每一帧
+#     ret, frame = cap.read()
+#     if not ret:
+#         logging.warning("_getPicture: Unable to read frames")
+#         continue
+#     cv2.imshow(WIN_NAME, frame)
+#     # 等待按键 s
+#     theKey = cv2.waitKey(1)
